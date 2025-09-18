b44bfe16810ae276992a0300c35c373a6a245016
mohnnnnnn <mohanram14159@gmail.com>
2025-09-18 17:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle between light and dark mode
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
