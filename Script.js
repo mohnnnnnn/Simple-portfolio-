e4cd47d3496243e13e7abe3e5c6fb7c446399a0f
mohnnnnnn <mohanram14159@gmail.com>
2025-09-18 15:01:18 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-// Toggle between light and dark mode
-const themeToggle = document.getElementById("themeToggle");
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
