2a5b4b99779565c9c6443e3f47f0e6ef876cf70a
mohnnnnnn <mohanram14159@gmail.com>
2025-09-18 15:26:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Simple form message display
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('formMsg').classList.remove('hidden');
-  this.reset();
-});
